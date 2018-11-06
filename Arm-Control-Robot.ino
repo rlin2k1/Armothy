1588abb724d3a398e4543889080141bd442c2a15
tristanmelton <30941629+tristanmelton@users.noreply.github.com>
2018-11-06 13:27:12 -0800

    Added second IMU and flex sensor

--- Arm-Control-Robot.ino
+++ Arm-Control-Robot.ino
@@ -1,17 +1,26 @@
+/**
+ * TCA9548 I2CScanner.pde -- I2C bus scanner for Arduino
+ *
+ * Based on code c. 2009, Tod E. Kurt, http://todbot.com/blog/
+ *
+ */
+ 
 #include <Wire.h>
 #include <SPI.h>
 #include <SparkFunLSM9DS1.h>
-//#include <SoftwareSerial.h>
+extern "C" { 
+#include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
  
 #define TCAADDR 0x70
-#define LSM9DS1_M   0x1E // Would be 0x1C if SDO_M is LOW
-#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
 
-//SoftwareSerial BT(10, 11); // May have to configure these values (should be TX, RX)
 LSM9DS1 imu1;
 LSM9DS1 imu2;
-LSM9DS1 imu3;
- 
+const int FLEX_PIN = 0;
+const float VCC = 5.02;
+const float R_DIV = 47000.0;
+const float STRAIGHT_RESIST = 37300.0;
+const float ANGLE_RESIST = 90000.0;
 void tcaselect(uint8_t i) {
   if (i > 7) return;
  
@@ -21,52 +30,63 @@ void tcaselect(uint8_t i) {
 }
  
  
-void setup(void) 
+// standard Arduino setup()
+void setup()
 {
-  Serial.begin(9600);  
-  Wire.begin();
-  //BT.begin(9600);
-  //Use these to configure settings on each imu
-  //imu.settings.accel.scale = 16; // Set accel range to +/-16g
-  //imu.settings.gyro.scale = 2000; // Set gyro range to +/-2000dps
-  //imu.settings.mag.scale = 8; // Set mag range to +/-8Gs
-  /* Initialise the 1st sensor */
+    while (!Serial);
+    delay(1000);
+ 
+    Wire.begin();
+    
+    Serial.begin(115200);
+    pinMode(FLEX_PIN, INPUT);
+    Serial.println("\nTCAScanner ready!");
+    
+    for (uint8_t t=0; t<8; t++) {
+      tcaselect(t);
+      Serial.print("TCA Port #"); Serial.println(t);
+ 
+      for (uint8_t addr = 0; addr<=127; addr++) {
+        if (addr == TCAADDR) continue;
+      
+        uint8_t data;
+        if (! twi_writeTo(addr, &data, 0, 1, 1)) {
+           Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
+        }
+      }
+    }
+    Serial.println("\ndone");
+
+  imu1.settings.device.commInterface = IMU_MODE_I2C; // Set mode to I2C 
+  imu1.settings.device.mAddress = 0x1E; // Set mag address to 0x1E
+  imu1.settings.device.agAddress = 0x6B; // Set ag address to 0x6B
+
+  imu2.settings.device.commInterface = IMU_MODE_I2C; // Set mode to I2C 
+  imu2.settings.device.mAddress = 0x1E; // Set mag address to 0x1E
+  imu2.settings.device.agAddress = 0x6B; // Set ag address to 0x6B
   tcaselect(2);
-  imu1.settings.device.commInterface = IMU_MODE_I2C;
-  imu1.settings.device.mAddress = LSM9DS1_M;
-  imu1.settings.device.agAddress = LSM9DS1_AG;
   if(!imu1.begin())
-  {
-    Serial.println("Failed to communicate with IMU 1.");
-    while(true);
-  }
-  /* Initialise the 2nd sensor */
-  /*tcaselect(5);
-  imu2.settings.device.commInterface = IMU_MODE_I2C;
-  imu2.settings.device.mAddress = LSM9DS1_M;
-  imu2.settings.device.agAddress = LSM9DS1_AG;
+    Serial.println("failed to communicate with IMU2!");
+  tcaselect(4);
   if(!imu2.begin())
-  {
-    Serial.println("Failed to communicate with IMU 2.");
-    while(true);
-  }*/
-  /* Initialise the 3rd sensor */
-  /*tcaselect(6);
-  imu3.settings.device.commInterface = IMU_MODE_I2C;
-  imu3.settings.device.mAddress = LSM9DS1_M;
-  imu3.settings.device.agAddress = LSM9DS1_AG;
-  if(!imu3.begin())
-  {
-    Serial.println("Failed to communicate with IMU 3.");
-    while(true);
-  }*/
+    Serial.println("Failed to communicate with IMU4!");
+    
 }
  
-void loop(void) 
+void loop() 
 {
-  //Use BT.println(Data_to_be_sent); to transmit data.
-  //Use if(BT.available()) to check if data has arrived in the bluetooth serial
-  //Use data_type (might have to be char) d = BT.read();
+  int flexADC = analogRead(FLEX_PIN);
+  float flexV = flexADC * VCC / 1023.0;
+  float flexR = R_DIV * (VCC / flexV - 1.0);
+  Serial.println("Resistance: " + String(flexR) + " ohms");
+
+  float angle = map(flexR, STRAIGHT_RESIST, ANGLE_RESIST,
+                   0, 90.0);
+  Serial.println("Bend: " + String(angle) + " degrees");
+  Serial.println();
+
+
+  
   tcaselect(2);
   imu1.readAccel();
   imu1.readGyro();
@@ -76,24 +96,16 @@ void loop(void)
   Serial.print("X: "); Serial.print(imu1.calcAccel(imu1.ax)); Serial.print(" "); Serial.println(imu1.calcGyro(imu1.gx));
   Serial.print("Y: "); Serial.print(imu1.calcAccel(imu1.ay)); Serial.print("  "); Serial.println(imu1.calcGyro(imu1.gy));
   Serial.print("Z: "); Serial.print(imu1.calcAccel(imu1.az)); Serial.print("  "); Serial.println(imu1.calcGyro(imu1.gz));
+
   
-  /*tcaselect(5);
-  imu1.readAccel();
-  imu1.readGyro();
+  tcaselect(4);
+  imu2.readAccel();
+  imu2.readGyro();
  
+  /* Display the results */
   Serial.print("Sensor #2 - ");
   Serial.print("X: "); Serial.print(imu2.calcAccel(imu2.ax)); Serial.print(" "); Serial.println(imu2.calcGyro(imu2.gx));
   Serial.print("Y: "); Serial.print(imu2.calcAccel(imu2.ay)); Serial.print("  "); Serial.println(imu2.calcGyro(imu2.gy));
   Serial.print("Z: "); Serial.print(imu2.calcAccel(imu2.az)); Serial.print("  "); Serial.println(imu2.calcGyro(imu2.gz));
-
-  tcaselect(6);
-  imu3.readAccel();
-  imu3.readGyro();
- 
-  Serial.print("Sensor #3 - ");
-  Serial.print("X: "); Serial.print(imu3.calcAccel(imu3.ax)); Serial.print(" "); Serial.println(imu3.calcGyro(imu3.gx));
-  Serial.print("Y: "); Serial.print(imu3.calcAccel(imu3.ay)); Serial.print("  "); Serial.println(imu3.calcGyro(imu3.gy));
-  Serial.print("Z: "); Serial.print(imu3.calcAccel(imu3.az)); Serial.print("  "); Serial.println(imu3.calcGyro(imu3.gz));
-  */
   delay(500);
 }
