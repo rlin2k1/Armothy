94efef00e3485c82b03cb93addbc280ebe293a73
Tristan Melton <tristan.melton.tm@gmail.com>
2018-11-11 15:51:05 -0800

    Data transmission

--- Arm-Control-Robot.ino
+++ Arm-Control-Robot.ino
@@ -1,17 +1,26 @@
+#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SparkFunLSM9DS1.h>
-//#include <SoftwareSerial.h>
- 
-#define TCAADDR 0x70
-#define LSM9DS1_M   0x1E // Would be 0x1C if SDO_M is LOW
-#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+extern "C" { 
+#include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
 
-//SoftwareSerial BT(10, 11); // May have to configure these values (should be TX, RX)
+#define TCAADDR 0x70
+
+SoftwareSerial BT(10,11);
+
 LSM9DS1 imu1;
 LSM9DS1 imu2;
-LSM9DS1 imu3;
- 
+
+String packet;
+
+const int FLEX_PIN = 0;
+const float VCC = 5.02;
+const float R_DIV = 47000.0;
+const float STRAIGHT_RESIST = 37300.0;
+const float ANGLE_RESIST = 90000.0;
+
 void tcaselect(uint8_t i) {
   if (i > 7) return;
  
@@ -19,81 +28,68 @@ void tcaselect(uint8_t i) {
   Wire.write(1 << i);
   Wire.endTransmission();  
 }
- 
- 
-void setup(void) 
-{
-  Serial.begin(9600);  
-  Wire.begin();
-  //BT.begin(9600);
-  //Use these to configure settings on each imu
-  //imu.settings.accel.scale = 16; // Set accel range to +/-16g
-  //imu.settings.gyro.scale = 2000; // Set gyro range to +/-2000dps
-  //imu.settings.mag.scale = 8; // Set mag range to +/-8Gs
-  /* Initialise the 1st sensor */
-  tcaselect(2);
-  imu1.settings.device.commInterface = IMU_MODE_I2C;
-  imu1.settings.device.mAddress = LSM9DS1_M;
-  imu1.settings.device.agAddress = LSM9DS1_AG;
-  if(!imu1.begin())
-  {
-    Serial.println("Failed to communicate with IMU 1.");
-    while(true);
-  }
-  /* Initialise the 2nd sensor */
-  /*tcaselect(5);
-  imu2.settings.device.commInterface = IMU_MODE_I2C;
-  imu2.settings.device.mAddress = LSM9DS1_M;
-  imu2.settings.device.agAddress = LSM9DS1_AG;
-  if(!imu2.begin())
-  {
-    Serial.println("Failed to communicate with IMU 2.");
-    while(true);
-  }*/
-  /* Initialise the 3rd sensor */
-  /*tcaselect(6);
-  imu3.settings.device.commInterface = IMU_MODE_I2C;
-  imu3.settings.device.mAddress = LSM9DS1_M;
-  imu3.settings.device.agAddress = LSM9DS1_AG;
-  if(!imu3.begin())
-  {
-    Serial.println("Failed to communicate with IMU 3.");
-    while(true);
-  }*/
-}
- 
-void loop(void) 
-{
-  //Use BT.println(Data_to_be_sent); to transmit data.
-  //Use if(BT.available()) to check if data has arrived in the bluetooth serial
-  //Use data_type (might have to be char) d = BT.read();
-  tcaselect(2);
-  imu1.readAccel();
-  imu1.readGyro();
- 
-  /* Display the results */
-  Serial.print("Sensor #1 - ");
-  Serial.print("X: "); Serial.print(imu1.calcAccel(imu1.ax)); Serial.print(" "); Serial.println(imu1.calcGyro(imu1.gx));
-  Serial.print("Y: "); Serial.print(imu1.calcAccel(imu1.ay)); Serial.print("  "); Serial.println(imu1.calcGyro(imu1.gy));
-  Serial.print("Z: "); Serial.print(imu1.calcAccel(imu1.az)); Serial.print("  "); Serial.println(imu1.calcGyro(imu1.gz));
-  
-  /*tcaselect(5);
-  imu1.readAccel();
-  imu1.readGyro();
- 
-  Serial.print("Sensor #2 - ");
-  Serial.print("X: "); Serial.print(imu2.calcAccel(imu2.ax)); Serial.print(" "); Serial.println(imu2.calcGyro(imu2.gx));
-  Serial.print("Y: "); Serial.print(imu2.calcAccel(imu2.ay)); Serial.print("  "); Serial.println(imu2.calcGyro(imu2.gy));
-  Serial.print("Z: "); Serial.print(imu2.calcAccel(imu2.az)); Serial.print("  "); Serial.println(imu2.calcGyro(imu2.gz));
 
-  tcaselect(6);
-  imu3.readAccel();
-  imu3.readGyro();
- 
-  Serial.print("Sensor #3 - ");
-  Serial.print("X: "); Serial.print(imu3.calcAccel(imu3.ax)); Serial.print(" "); Serial.println(imu3.calcGyro(imu3.gx));
-  Serial.print("Y: "); Serial.print(imu3.calcAccel(imu3.ay)); Serial.print("  "); Serial.println(imu3.calcGyro(imu3.gy));
-  Serial.print("Z: "); Serial.print(imu3.calcAccel(imu3.az)); Serial.print("  "); Serial.println(imu3.calcGyro(imu3.gz));
-  */
-  delay(500);
+void setup() {
+  Wire.begin();
+  BT.begin(38400);
+
+  imu1.settings.device.commInterface = IMU_MODE_I2C; // Set mode to I2C 
+  imu1.settings.device.mAddress = 0x1E; // Set mag address to 0x1E
+  imu1.settings.device.agAddress = 0x6B; // Set ag address to 0x6B
+
+  imu2.settings.device.commInterface = IMU_MODE_I2C; // Set mode to I2C 
+  imu2.settings.device.mAddress = 0x1E; // Set mag address to 0x1E
+  imu2.settings.device.agAddress = 0x6B; // Set ag address to 0x6B
+  tcaselect(2);
+  if(!imu1.begin())
+    Serial.println("failed to communicate with IMU2!");
+  tcaselect(4);
+  if(!imu2.begin())
+    Serial.println("Failed to communicate with IMU4!");
+  packet = "";
+}
+
+void loop() 
+{
+  int flexADC = analogRead(FLEX_PIN);
+  float flexV = flexADC * VCC / 1023.0;
+  float flexR = R_DIV * (VCC / flexV - 1.0);
+
+  float angle = map(flexR, STRAIGHT_RESIST, ANGLE_RESIST,
+                   0, 90.0);
+  packet += "a,";
+  packet += String(angle); packet += ',';
+  tcaselect(2);
+  imu1.readAccel();
+  imu1.readGyro();
+  imu1.readMag();
+  packet += String(imu1.calcAccel(imu1.ax)); packet += ',';
+  packet += String(imu1.calcAccel(imu1.ay)); packet += ',';
+  packet += String(imu1.calcAccel(imu1.az)); packet += ',';
+  packet += String(imu1.calcGyro(imu1.gx)); packet += ',';
+  packet += String(imu1.calcGyro(imu1.gy)); packet += ',';
+  packet += String(imu1.calcGyro(imu1.gz)); packet += ',';
+  packet += String(imu1.calcMag(imu1.mx)); packet += ',';
+  packet += String(imu1.calcMag(imu1.my)); packet += ',';
+  packet += String(imu1.calcMag(imu1.mz));
+  
+  // put your main code here, to run repeatedly:
+  BT.println(packet);
+  packet = "b,";
+  tcaselect(4);
+  imu2.readAccel();
+  imu2.readGyro();
+  imu2.readMag();
+  packet += String(imu2.calcAccel(imu2.ax)); packet += ',';
+  packet += String(imu2.calcAccel(imu2.ay)); packet += ',';
+  packet += String(imu2.calcAccel(imu2.az)); packet += ',';
+  packet += String(imu2.calcGyro(imu2.gx)); packet += ',';
+  packet += String(imu2.calcGyro(imu2.gy)); packet += ',';
+  packet += String(imu2.calcGyro(imu2.gz)); packet += ',';
+  packet += String(imu2.calcMag(imu2.mx)); packet += ',';
+  packet += String(imu2.calcMag(imu2.my)); packet += ',';
+  packet += String(imu2.calcMag(imu2.mz));
+  BT.println(packet);
+  packet = "";
+  delay(100);
 }
